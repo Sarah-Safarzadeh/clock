2883bc976cabf84519e9945fd66d96e249ee4128
Sarah Safarzadeh <Sarah-Safarzadeh@users.noreply.github.com>
2021-03-31 23:51:11 -0500

    added setRotation function, called setClock

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // calls setClock function every second
 setInterval(setClock, 1000);
 
+const hourHand = document.querySelector('[data-hour-hand');
+const minuteHand = document.querySelector('[data-minute-hand');
+const secondHand = document.querySelector('[data-second-hand');
+
 function setClock() {
     const currentDate = new Date()
 
@@ -9,4 +13,14 @@ function setClock() {
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
 
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
 };
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+};
+
+setClock();
